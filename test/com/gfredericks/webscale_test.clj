219ad6393375a4bbd7c26253d8cb0c358a17c0df
Gary Fredericks <fredericksgary@gmail.com>
2015-10-04 16:55:50 -0500

    Add a parallel test

--- test/com/gfredericks/webscale_test.clj
+++ test/com/gfredericks/webscale_test.clj
@@ -3,7 +3,8 @@
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
             [com.gfredericks.webscale :as webscale]
-            [me.raynes.fs :as fs]))
+            [me.raynes.fs :as fs])
+  (:import (java.util.concurrent Executors)))
 
 (def gen-key (gen/elements (->> (range (int \a) (inc (int \z)))
                                 (map char)
@@ -40,3 +41,29 @@
 (defspec lots-of-events-spec 5
   (prop/for-all [evs (gen/scale #(* 10000 %) (gen/list gen-event))]
     (events-go-good evs)))
+
+(defn test-events-in-parallel
+  [events nthreads]
+  (let [dir (fs/temp-dir "webscale-test")]
+    (try
+      (let [the-thing (webscale/create incr-by-key {} dir)
+            expected-end-state (reduce incr-by-key {} events)
+            pool (Executors/newFixedThreadPool nthreads)]
+        (try
+          (let [futs
+                (.invokeAll pool (map #(partial webscale/update! the-thing %) events))]
+            (doseq [fut futs] (deref fut)))
+          (let [in-memory-end-state @the-thing
+                re-reading-end-state @(webscale/create incr-by-key {} dir)]
+            (= expected-end-state
+               in-memory-end-state
+               re-reading-end-state))
+          (finally
+            (.shutdown pool))))
+      (finally
+        (fs/delete-dir dir)))))
+
+(defspec parallel-events-spec 10
+  (prop/for-all [evs (gen/scale #(* 40 %) (gen/list gen-event))
+                 nthreads gen/s-pos-int]
+    (test-events-in-parallel evs nthreads)))
