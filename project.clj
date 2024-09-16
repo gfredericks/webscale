bdf0a1971ad1919b520a05f0ea1e2b206792022c
Gary Fredericks <fredericksgary@gmail.com>
2024-09-15 19:01:07 -0500

    Upgrade deps to fix tests

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/gfredericks/webscale"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [mvxcvi/puget "1.0.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [mvxcvi/puget "1.3.4"]]
   :deploy-repositories [["releases" :clojars]]
   :profiles {:dev {:dependencies [[me.raynes/fs "1.4.6"]
                                   [org.clojure/test.check "0.10.0-alpha3"]
